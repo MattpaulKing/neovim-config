d0b5148c9f38af241e8dfda3137a3e2762ce20e6
Matt King <matthewpaulking@outlook.com>
2024-01-04 03:39:11 -0800

    feat: keybinds for git

--- after/plugin/fugitive.lua
+++ after/plugin/fugitive.lua
@@ -1,1 +1,20 @@
 vim.keymap.set("n", "<leader>gs", vim.cmd.Git);
+
+local my_fugitive = vim.api.nvim_create_augroup("my_fugitive", {})
+
+local autocmd = vim.api.nvim_create_autocmd
+autocmd("BufWinEnter", {
+  group = my_fugitive,
+  pattern = "*",
+  callback = function()
+    if vim.bo.ft ~= "fugitive" then
+      return
+    end
+
+    local bufnr = vim.api.nvim_get_current_buf()
+    local opts = { buffer = bufnr, remap = false }
+    vim.keymap.set("n", "<leader>p", function()
+      vim.cmd.Git("push")
+    end, opts)
+  end,
+})
